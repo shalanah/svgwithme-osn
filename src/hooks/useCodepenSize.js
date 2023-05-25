6dc1fd2c6f54e40bb54432057b05b60c6c48db74
shalanah <shalanahfaith@gmail.com>
2023-05-24 22:49:47 -0500

    Alter scale

--- src/hooks/useCodepenSize.js
+++ src/hooks/useCodepenSize.js
@@ -2,7 +2,7 @@ import useWinSize from "./useWinSize";
 
 const useCodepenSize = ([t = 0, r = 0, b = 0, l = 0]) => {
   const dim = useWinSize();
-  const scale = 1.9; // to make more legible
+  const scale = 1.65; // to make more legible
   const width = (dim.width - (r + l)) / scale;
   const height = (dim.height - (t + b)) / scale;
   return [width, height, scale];
